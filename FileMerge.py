e99cb7b96f0f3b97c0cb2418ec8228253e1e1471
Marirs27 <68460929+Marirs27@users.noreply.github.com>
2021-02-26 19:06:24 +0530

    Add files via upload

--- FileMerge.py
+++ FileMerge.py
@@ -1,18 +1,22 @@
 import pandas as pd
 import sys
+import glob
 
-if ( len(sys.argv) != 4 ) :
+if ( len(sys.argv) != 3 ) :
     print("Please provide all three arguments!")
 else :
-    data1 = pd.read_csv(sys.argv[1])
-    data2 = pd.read_csv(sys.argv[2])
-    data3 = pd.concat([data1,data2],axis=1, join="inner")
+    path = sys.argv[1]
+    all_files = glob.glob(path + "/*.csv")
+    li = []
+    for filename in all_files :
+        df = pd.read_csv(filename, index_col=None, header=0)
+        li.append(df)
+    data3 = pd.concat(li, axis=1, join="inner")
     for column in range(len(data3.columns)) :
         for col in data3.columns[column+1:] :
             if data3[data3.columns[column]].equals(data3[col]) :
                 del data3[col]
     if not data3.empty :
-        data3.to_csv(sys.argv[3])
+        data3.to_csv(sys.argv[2])
     else :
-        print("The file is Empty!")
-
+        print("All columns were repeated !")
