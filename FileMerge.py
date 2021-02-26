c241aa9615450a7f6992fa7deef2165bd417d7e9
Marirs27 <68460929+Marirs27@users.noreply.github.com>
2021-02-26 11:29:10 +0530

    Add files via upload

---
+++ FileMerge.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import sys
+
+if ( len(sys.argv) != 4 ) :
+    print("Please provide all three arguments!")
+else :
+    data1 = pd.read_csv(sys.argv[1])
+    data2 = pd.read_csv(sys.argv[2])
+    data3 = pd.concat([data1,data2],axis=1, join="inner")
+    for column in range(len(data3.columns)) :
+        for col in data3.columns[column+1:] :
+            if data3[data3.columns[column]].equals(data3[col]) :
+                del data3[col]
+    if not data3.empty :
+        data3.to_csv(sys.argv[3])
+    else :
+        print("All columns were repeated !")
+
